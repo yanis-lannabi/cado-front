574ade01f90cdc1db6f8c826be76d9dc7da3c71b
EstelleTroadec <estelle.troadec@oclock.school>
2024-07-01 19:47:21 +0200

    feat: beginning to add the componentspreparing the components in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './components/App/App';
+// import HomePage from './components/Pages/HomePage/HomePage';
+// import CreateEvent from './components/Pages/CreateEvent/CreateEvent';
+import DrawResult from './components/Pages/DrawResult/DrawResult';
+// import EventDetails from './components/Pages/EventDetails/EventDetails';
+// import FAQ from './components/Pages/FAQ/FAQ';
+// import LegalNotices from './components/Pages/LegalNotices/LegalNotices';
+// import Login from './components/Pages/Login/Login';
+// import MyAccount from './components/Pages/MyAccount/MyAccount';
+// import MyEvent from './components/Pages/MyEvent/MyEvent';
+// import PersonnalData from './components/Pages/PersonnalData/PersonnalData';
+// import SignUp from './components/Pages/SignUp/SignUp';
 
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <DrawResult />
   </React.StrictMode>
 );
