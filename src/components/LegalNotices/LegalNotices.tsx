ba4c5abc8acb2483fd90be56334a34f456afa247
AnaisSERY <anais.sery@oclock.school>
2024-07-01 11:46:12 +0200

    hard code for FAQ, LegalNotices, Login and MyAccount

---
+++ src/components/LegalNotices/LegalNotices.tsx
@@ -0,0 +1,51 @@
+import './LegalNotices.scss';
+
+function LegalNotices() {
+  return (
+    <article className="LegalNotices">
+      <h2 className="LegalNotices-title">LegalNotices</h2>
+      <div className="LegalNotices-category" />
+      <p className="LegalNotices-content">
+        Privacy Policy Last updated on 18/11/2022 Thank you for visiting
+        secretsantaorganizer.com created by iO Group (2200 Herentals, Zavelheide
+        15, BE0712.677.311) (hereafter ‘we’ ‘us’). This privacy policy tells you
+        how we use personal information collected while using the site
+        secretsantaorganizer.com (hereafter ‘Website’) and the related services
+        (hereafter “Services”). By using our Website and/or our Services, you
+        acknowledge that you have carefully read this Privacy Policy.We reserve
+        the right to regularly change the Privacy Policy as we deem fit. Such
+        change will be communicated through the website. Note: the privacy
+        practices set forth in this privacy policy are only applicable for this
+        Website and the Services . If you link to other web sites, it’s advised
+        to review the privacy policies on those sites. Cookie/Tracking
+        Technology This Website uses cookies present in Google Analytics for
+        gathering anonymous information such as browser type and operating
+        tracking the number of visitors to the site, and understanding how
+        visitors use the site. For more information regarding our use of
+        cookies, please read our cookie policy. Collection of Information We
+        collect personally identifiable information, like names and email
+        addresses and personal messages, when submitted by our visitors with
+        their permission. As administrator of a party you guarantee that you
+        have the consent of the other participants to share their e-mail
+        addresses for the use of Secret Santa tool. If you declare you have
+        obtained this consent we can reasonably assume that you actually
+        obtained this consent. The information you provide is only used to
+        fulfill your specific request and the use of the Services of Secret
+        principle, we obtain this above-mentioned personal data directly from
+        you. We do not send any personal data that you provide through social
+        social media providers, unless you agree to this. If and If and your
+        your registration on the Website or use of the Website or Services can
+        regarded (a) as a breach of the terms and conditions of the intellectual
+        property rights or any other right of a third party, (b) a threat to the
+        security or integrity of the Services, (c) a danger to our or our
+        subcontractors’ Website, Services or systems as a result of viruses,
+        Trojan Horses, spyware, malware or any other form of malicious code, or
+        (d) in any manner whatsoever illegal, unlawful, discriminatory or
+        insulting, we may process your data in our own interests, in our in our
+        partners’ or in third parties’ interests.
+      </p>
+    </article>
+  );
+}
+
+export default LegalNotices;
