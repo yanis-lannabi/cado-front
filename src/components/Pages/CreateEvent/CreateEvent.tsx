5a4c553f1072eef9e7f369fe9cdde8fee63efb96
EstelleTroadec <estelle.troadec@oclock.school>
2024-07-01 17:48:46 +0200

    feat: finishing to add the elements + adding some style

--- src/components/Pages/CreateEvent/CreateEvent.tsx
+++ src/components/Pages/CreateEvent/CreateEvent.tsx
@@ -2,56 +2,64 @@ import './CreateEvent.scss';
 
 function CreateEvent() {
   return (
-    <div className="create-event">
-      <header className="header">
-        <img src="../../assets/logo.svg" alt="logo" />
-        <button>Déconnexion</button>
-      </header>
+    <div className="create-event-page">
+      <h2>Créer mon évènement</h2>
 
-      <h1>Créer mon évènement</h1>
+      <form className="create-event">
+        <div className="create-event__element">
+          <h3>Nom de l'évènement :</h3>
+          <input type="text" placeholder="Nom de l'évènement" />
+        </div>
+        <div className="create-event__element">
+          <h3>Date de l'évènement :</h3>
+          <input type="text" placeholder="JJ/MM/AAAA" />
+        </div>
+        <div className="create-event__element">
+          <h3>Description :</h3>
+          <input type="text" placeholder="Description" />
+        </div>
+        <div className="create-event__element">
+          <h3>Participants :</h3>
 
-      <div className="create-event__name">
-        <h2>Nom de l'évènement</h2>
-        <input type="text" placeholder="Nom de l'évènement" />
-      </div>
-      <div className="create-event__date">
-        <h2>Date de l'évènement</h2>
-        <input type="text" placeholder="JJ/MM/AAAA" />
-      </div>
-      <div className="create-event__description">
-        <h2>Description</h2>
-        <input type="text" placeholder="Description" />
-      </div>
-      <div className="create-event__participants">
-        <h2>Participants</h2>
-        <input
-          className="create-event__participants__input-name1"
-          type="text"
-          placeholder="Nom du participant 1"
-        />
-        <input
-          className="create-event__participants__input-email1"
-          type="text"
-          placeholder="E-mail du participant 1"
-        />
-        <input
-          className="create-event__participants__input-name2"
-          type="text"
-          placeholder="Nom du participant 2"
-        />
-        <input
-          className="create-event__participants__input-email2"
-          type="text"
-          placeholder="E-mail du participant 2"
-        />
-        <button>
-          Ajouter un participant <span>+</span>
+          <div className="create-event__participants">
+            <div className="create-event__participant">
+              <input
+                className="create-event__participant__input-name"
+                type="text"
+                placeholder="Nom du participant"
+              />
+              <input
+                className="create-event__participant__input-email"
+                type="text"
+                placeholder="E-mail du participant"
+              />
+            </div>
+            <div className="create-event__participant">
+              <input
+                className="create-event__participant__input-name"
+                type="text"
+                placeholder="Nom du participant"
+              />
+              <input
+                className="create-event__participant__input-email"
+                type="text"
+                placeholder="E-mail du participant"
+              />
+            </div>
+            <button className="create-event__participants__add-button">
+              +
+            </button>
+          </div>
+        </div>
+        <div className="create-event__element">
+          <h3>Budget :</h3>
+          <input type="text" placeholder="Budget maximum par cadeau" />
+        </div>
+
+        <button className="create-event__validation-button">
+          Enregistrer les modifications
         </button>
-      </div>
-      <div className="create-event__budget">
-        <h2>Budget</h2>
-        <input type="text" placeholder="Somme maximum" />
-      </div>
+      </form>
     </div>
   );
 }
