4b949f439b2d0280cb2764354981062c719ddddd
AnaisSERY <anais.sery@oclock.school>
2024-07-01 15:59:14 +0200

    build: update hard code on Login

--- src/components/Pages/Login/Login.tsx
+++ src/components/Pages/Login/Login.tsx
@@ -1,13 +1,33 @@
 import './Login.scss';
+//import Header from './.../Element/Header/Header';
+//import Footer from './.../Element/Footer/Footer';
 
 function Login() {
   return (
-    <div className="Login">
-      <header className="Login-header">
-        <h1>Connectez-vous à votre compte</h1>
+    <div className="WebsiteName">
+      <header className="Website__Title">
+        <h1>Cad'O</h1>
       </header>
-      <p>email</p>
-      <p>mot de passe</p>
+      <div className="Login">
+        <h2>Connectez-vous à votre compte</h2>
+        <form>
+          <div className="Login__email">
+            <input type="email" placeholder="email" />
+          </div>
+          <div className="Login__password">
+            <input type="password" placeholder="password" />
+          </div>
+          <button className="Login__confirmation" type="submit">
+            Continuer
+          </button>
+        </form>
+        <p className="Login__newAccount">
+          ------Pas de compte, Créez-en un------
+        </p>
+        <button className="Login__createAccount" type="submit">
+          Créer un compte
+        </button>
+      </div>
     </div>
   );
 }
