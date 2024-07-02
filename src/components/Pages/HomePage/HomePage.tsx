236d7f4c4536f7a201c884fcafb2eb738a18ccc5
EstelleTroadec <estelle.troadec@oclock.school>
2024-07-02 11:54:19 +0200

    feat: creating the home page + adding some style

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -1,26 +1,29 @@
-import logo from '../../../../public/MainLogo.png';
+import homeImage from '../../../../public/FavIcon.png';
 
-import './App.scss';
+import './HomePage.scss';
 
 function HomePage() {
   return (
-    <div className="HomePage">
-      <header className="HomePage-header">
-        <img src={logo} className="HomePage-logo" alt="logo" />
+    <div className="home">
+      <h2 className="home__title">Bienvenue chez Cad'O</h2>
 
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          BLABLA
-        </a>
-      </header>
+      <div className="home__container">
+        <div className="home__concept-explaination">
+          <h3>Mais qu'est-ce que Cad'O ?</h3>
+          <p>
+            Le concept du Secret Santa est un moyen de s’offrir des cadeaux de
+            façon anonyme : à l’aide d’un tirage au sort, chaque participant se
+            voit attribuer par tirage au sort une personne à qui il offrira un
+            cadeau. La personne qui reçoit le cadeau ne connaît pas l’identité
+            de celui qui lui offre.
+            <br />
+            <br />
+            Avec Cad'O, nous avons donc créé un outil qui permettra de faciliter
+            l'organisation de vos Secret Santa !
+          </p>
+        </div>
+        <img className="home__image" src={homeImage} alt="" />
+      </div>
     </div>
   );
 }
