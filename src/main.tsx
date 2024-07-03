eaa91a7eb843e631683961392cd045f3bc8ce38c
EstelleTroadec <estelle.troadec@oclock.school>
2024-07-03 13:17:12 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import MyEvent from './components/Pages/MyEvent/MyEvent';
+import CreateEvent from './components/Pages/CreateEvent/CreateEvent';
+import Header from './components/Elements/Header/Header';
 
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MyEvent />
+    <Header />
+    <CreateEvent />
   </React.StrictMode>
 );
