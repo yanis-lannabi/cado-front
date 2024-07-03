4a53c7eb60b1ab5e7af978005eec474ef90839f1
EstelleTroadec <estelle.troadec@oclock.school>
2024-07-03 22:08:40 +0200

    feature: implementing states for every field of the form + begginning to handle onSubmit and onClick events

--- src/components/Pages/CreateEvent/CreateEvent.tsx
+++ src/components/Pages/CreateEvent/CreateEvent.tsx
@@ -1,62 +1,143 @@
+import { useState } from 'react';
 import './CreateEvent.scss';
 
+// Fictive participants list, in order to test the form
+const participants = [
+  {
+    name: 'Bob',
+    email: 'bobby@bobby.fr',
+  },
+  {
+    name: 'Michel',
+    email: 'michou@michel.fr',
+  },
+  {
+    name: 'Roberto',
+    email: 'Rob@roberto.fr',
+  },
+  {
+    name: 'Mcikey',
+    email: 'mickey@mickey.fr',
+  },
+  {
+    name: 'Shakira',
+    email: 'shaki@shaki.fr',
+  },
+];
+
 function CreateEvent() {
+  const [eventName, setEventName] = useState('');
+  const [eventDate, setEventDate] = useState('');
+  const [eventDescription, setEventDescription] = useState('');
+  const [participant, setParticipant] = useState([]);
+  const [giftBudget, setGiftBudget] = useState('');
+
+  const addParticipantLine = (e) => {
+    e.preventDefault();
+    return (
+      <div className="create-event__element">
+        <input type="text" id="participant" placeholder="Participant" />
+      </div>
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!eventName && !eventDate && !participant) {
+      alert('Veuillez remplir tous les champs obligatoires');
+      return;
+    }
+    alert(`Youhou ! Votre évènement "${eventName}" a bien été créé ! :)`);
+
+    setEventName('');
+    setEventDate('');
+    setEventDescription('');
+    setParticipant([]);
+    setGiftBudget('');
+  };
+
   return (
     <div className="create-event-page">
       <h2>Créer mon évènement</h2>
 
       <form className="create-event">
         <div className="create-event__element">
-          <h3>Nom de l'évènement :</h3>
-          <input type="text" placeholder="Nom de l'évènement" />
+          <h3>* Nom de l'évènement :</h3>
+          <input
+            type="text"
+            id="eventName"
+            value={eventName}
+            placeholder="Nom de l'évènement"
+            onChange={(e) => setEventName(e.target.value)}
+          />
         </div>
+
         <div className="create-event__element">
-          <h3>Date de l'évènement :</h3>
-          <input type="text" placeholder="JJ/MM/AAAA" />
+          <h3>* Date de l'évènement :</h3>
+          <input
+            type="date"
+            id="eventDate"
+            value={eventDate}
+            onChange={(e) => setEventDate(e.target.value)}
+          />
         </div>
         <div className="create-event__element">
           <h3>Description :</h3>
-          <input type="text" placeholder="Description" />
+          <input
+            type="text"
+            id="eventDescription"
+            value={eventDescription}
+            placeholder="Description de l'évènement"
+            onChange={(e) => setEventDescription(e.target.value)}
+          />
         </div>
         <div className="create-event__element">
-          <h3>Participants :</h3>
+          <h3>* Participants :</h3>
 
           <div className="create-event__participants">
             <div className="create-event__participant">
               <input
-                className="create-event__participant__input-name"
                 type="text"
+                id="participantName"
                 placeholder="Nom du participant"
               />
               <input
-                className="create-event__participant__input-email"
-                type="text"
+                type="email"
+                id="participantEmail"
                 placeholder="E-mail du participant"
               />
             </div>
-            <div className="create-event__participant">
-              <input
-                className="create-event__participant__input-name"
-                type="text"
-                placeholder="Nom du participant"
-              />
-              <input
-                className="create-event__participant__input-email"
-                type="text"
-                placeholder="E-mail du participant"
-              />
-            </div>
-            <button className="create-event__participants__add-button">
+
+            <button
+              className="create-event__participants__add-button"
+              onClick={addParticipantLine}
+            >
               +
             </button>
           </div>
         </div>
         <div className="create-event__element">
           <h3>Budget :</h3>
-          <input type="text" placeholder="Budget maximum par cadeau" />
+          <input
+            type="number"
+            id="giftBudget"
+            value={giftBudget}
+            placeholder="Budget maximum par cadeau"
+          />
         </div>
 
-        <button className="create-event__validation-button">Valider</button>
+        <p className="create-event__mandatory-fields">
+          * Les champs avec une astérisque sont obligatoires
+        </p>
+
+        <button
+          type="submit"
+          className="create-event__validation-button"
+          onSubmit={handleSubmit}
+        >
+          Valider
+        </button>
       </form>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import CreateEvent from './components/Pages/CreateEvent/CreateEvent';
 import Header from './components/Elements/Header/Header';
+import Footer from './components/Elements/Footer/Footer';
 
+// import 'semantic-ui-css/semantic.min.css';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
     <CreateEvent />
+    <Footer />
   </React.StrictMode>
 );
