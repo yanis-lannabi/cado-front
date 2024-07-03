9aa5c2ed5af83c9411cd3442c6e9290b098b19f5
AnaisSERY <anais.sery@oclock.school>
2024-07-03 14:00:50 +0200

    implement login feature

--- src/components/Pages/FAQ/FAQ.tsx
+++ src/components/Pages/FAQ/FAQ.tsx
@@ -3,7 +3,7 @@ import './FAQ.scss';
 function FAQ() {
   return (
     <article className="FAQ">
-      <h2 className="FAQ-title">FAQ :</h2>
+      <h2 className="FAQ-title">FOIRE AUX QUESTIONS :</h2>
       <div className="FAQ-category">
         <h3> Qu'est-ce que Secret Santa?</h3>
         <p className="FAQ-content">
@@ -11,11 +11,11 @@ function FAQ() {
           ligne! Organisez une Secret Santa party avec des amis, de la famille
           ou même entre collègues. Chaque année, autour de la période de des
           fêtes, les personnes du monde entier s'échangent des cadeaux. Pour
-          Pour garder les choses intéressantes, vous pouvez aassocier une
+          Pour garder les choses intéressantes, vous pouvez associer une
           personne à une autre au hasard, afin de s'offrir des cadeaux.
         </p>
         <h3>
-          Comment puis-je gérer mon événement Secret Santa et les participants?
+          Comment puis-je gérer mon évènement Secret Santa et les participants?
         </h3>
         <p className="FAQ-content">
           L'e-mail de confirmation qui a été envoyé à l'administrateur contient
--- src/components/Pages/Login/Login.tsx
+++ src/components/Pages/Login/Login.tsx
@@ -1,21 +1,105 @@
 import './Login.scss';
-//import Header from './.../Element/Header/Header';
-//import Footer from './.../Element/Footer/Footer';
+import Header from '../../Elements/Header/Header';
+import Footer from '../../Elements/Footer/Footer';
+
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // Initializes the errors state with an empty string.
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (email && !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      setEmailError('Veuillez entrer un email valide.');
+    } else {
+      setEmailError('');
+    }
+
+    if (password && password.length < 7) {
+      setPasswordError('Le mot de passe doit être supérieur à 7 caractères');
+    } else {
+      setPasswordError('Entrez un mot de passe');
+    }
+  }, [email, password]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (emailError || passwordError) {
+      return;
+    }
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        // If the status if different from 200, it means that an error occured.
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Erreur lors de la connexion');
+      }
+      // Redirigez l'utilisateur vers la page "Mon compte"
+      navigate('/mon-compte');
+    } catch (error) {
+      if (error instanceof Error) {
+        // Si une erreur se produit lors de l'envoi de la requête,
+        setErrorMessage(error.message);
+        console.error(error);
+      }
+    }
+  };
+
+  const handleCreateAccount = () => {
+    // Redirect to create account page
+    //(à remplacer par la vraie URL de la page création de compte)
+    navigate('/creer-compte');
+  };
+
   return (
     <div className="WebsiteName">
+      <Header />
       <header className="Website__Title">
         <h1>Cad'O</h1>
       </header>
       <div className="Login">
         <h2>Connectez-vous à votre compte</h2>
-        <form>
+        {errorMessage && (
+          <div className="Login__errorMessage">{errorMessage}</div>
+        )}
+        {emailError && <div className="Login__emailError">{emailError}</div>}
+        {passwordError && (
+          <div className="Login__passwordError">{passwordError}</div>
+        )}
+        <form onSubmit={handleSubmit}>
           <div className="Login__email">
-            <input type="email" placeholder="email" />
+            <input
+              type="email"
+              placeholder="email"
+              value={email}
+              //Updates the email state with the user's input.
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
           <div className="Login__password">
-            <input type="password" placeholder="password" />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              //Updates the password state with the user's input.
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <button className="Login__confirmation" type="submit">
             Continuer
@@ -24,10 +108,11 @@ function Login() {
         <p className="Login__newAccount">
           ------Pas de compte, Créez-en un------
         </p>
-        <button className="Login__createAccount" type="submit">
+        <button className="Login__createAccount" onClick={handleCreateAccount}>
           Créer un compte
         </button>
       </div>
+      <Footer />
     </div>
   );
 }
