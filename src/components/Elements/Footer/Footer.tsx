a33be19cbf5036e8d88a96728b5a803a9f73d48f
AnaisSERY <anais.sery@oclock.school>
2024-07-03 18:18:02 +0200

    Revert "finalize My Account page"
    This reverts commit fb336340544e53d297854444a08c1abfdcaef235.

--- src/components/Elements/Footer/Footer.tsx
+++ src/components/Elements/Footer/Footer.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.scss'; // Importez votre fichier SCSS
+import './Footer.scss';
 
-const Footer: React.FC = () => {
+function Footer() {
   return (
     <footer className="footer">
-      <div>
-        <Link to="/mentions-legales">Mentions légales</Link>
-        <Link to="/faq">FAQ</Link>
-      </div>
-      <div>Version du site</div>
+      <p>© 2024 Company Name</p>
     </footer>
   );
-};
+}
 
 export default Footer;
