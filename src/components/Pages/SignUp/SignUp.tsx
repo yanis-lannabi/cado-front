bb8f7e613da51cdd950a82d800699434b36d4ce8
YoannDaSilvaCastro <yoann.dsc@gmail.com>
2024-07-03 14:32:49 +0200

    feat: :construction: SignUp feature
    Working on the SignUp feature, waiting to connect to the API.

--- src/components/Pages/SignUp/SignUp.tsx
+++ src/components/Pages/SignUp/SignUp.tsx
@@ -1,8 +1,38 @@
+/* eslint-disable no-console */
 import './SignUp.scss';
+import React, { useState } from 'react';
 // import Header from '../../Elements/Header/Header';
 // import Footer from '../../Elements/Footer/Footer';
 
 function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('name:', name);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // Perform signup logic here
+    // You can make an API call here to submit the form data to the server
+    // For example:
+    // fetch('/api/signup', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ email, password }),
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   // Handle the response from the server
+    // })
+    // .catch(error => {
+    //   // Handle any errors that occur during the API call
+    // });
+  };
+
   return (
     <div className="WebsiteName">
       <header className="Website__title">
@@ -10,19 +40,32 @@ function SignUp() {
       </header>
       <div className="SignUp">
         <h2>S'inscrire</h2>
-        <form>
-          <div className="SignUp__name">
-            Nom :
-            <input type="text" placeholder="Nom" />
-          </div>
-          <div className="SignUp__email">
-            Adresse email :
-            <input type="email" placeholder="Adresse mail" />
-          </div>
-          <div className="SignUp__password">
-            Mot de passe :
-            <input type="password" placeholder="Mot de passe" />
-          </div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nom:</label>
+          <input
+            className="SignUp__name"
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            className="SignUp__email"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            className="SignUp__password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
           <button className="SignUp__confirmation" type="submit">
             S'inscrire
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import MyEvent from './components/Pages/MyEvent/MyEvent';
+import SignUp from './components/Pages/SignUp/SignUp';
 
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MyEvent />
+    <SignUp />
   </React.StrictMode>
 );
