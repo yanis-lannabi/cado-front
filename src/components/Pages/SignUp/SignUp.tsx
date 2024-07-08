6c4c30dfd4d684795397bdb0b616f03ab79d8bd5
AnaisSERY <anais.sery@oclock.school>
2024-07-08 10:20:10 +0200

    fixed login

--- src/components/Pages/SignUp/SignUp.tsx
+++ src/components/Pages/SignUp/SignUp.tsx
@@ -3,6 +3,44 @@ import Header from '../../Elements/Header/Header';
 import Footer from '../../Elements/Footer/Footer';
 
 function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signUpStatus, setSignUpStatus] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('name:', name);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // API call
+    fetch('http://165.227.232.51:3000/auth/register/', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.name && data.email && data.password) {
+          setSignUpStatus('Inscription réussie !');
+          setName('');
+          setEmail('');
+          setPassword('');
+        } else {
+          setSignUpStatus("Échec de l'inscription : Données manquantes");
+        }
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+        setSignUpStatus("Échec de l'inscription : une erreur est survenue");
+        setName('');
+        setEmail('');
+        setPassword('');
+      });
+  };
+
   return (
     <div className="SignUp">
       <Header />
