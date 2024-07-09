7c7c01815372e17389d0b992733d8b4c9c9d287c
AnaisSERY <anais.sery@oclock.school>
2024-07-09 16:49:28 +0200

    update on FAQ, Footer and Header. Disabled CSS cause malfuctions

--- src/components/Elements/Footer/Footer.tsx
+++ src/components/Elements/Footer/Footer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Footer.scss'; // Importez votre fichier SCSS
 
 const Footer: React.FC = () => {
+  const location = useLocation();
+
   return (
     <footer className="footer">
       <div>
-        <Link to="/mentions-legales">Mentions légales</Link>
-        <Link to="/faq">FAQ</Link>
+        {location.pathname !== '/mentions-legales' && (
+          <Link to="/mentions-legales">Mentions légales</Link>
+        )}
+        {location.pathname !== '/faq' && <Link to="/faq">FAQ</Link>}
         <div>© 2024</div>
       </div>
     </footer>
