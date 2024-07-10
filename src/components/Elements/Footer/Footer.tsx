83ac9fc8f194a54a899f1f09a13d825a7302a699
AnaisSERY <anais.sery@oclock.school>
2024-07-10 10:29:00 +0200

    modifications on header and footer to match dev

--- src/components/Elements/Footer/Footer.tsx
+++ src/components/Elements/Footer/Footer.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Footer.scss'; // Importez votre fichier SCSS
 
 const Footer: React.FC = () => {
-  const location = useLocation();
-
   return (
     <footer className="footer">
       <div>
-        {location.pathname !== '/mentions-legales' && (
-          <Link to="/mentions-legales">Mentions légales</Link>
-        )}
-        {location.pathname !== '/faq' && <Link to="/faq">FAQ</Link>}
+        <Link to="/mentions-legales">Mentions légales</Link>
+        <Link to="/faq">FAQ</Link>
         <div>© 2024</div>
       </div>
     </footer>
