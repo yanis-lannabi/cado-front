53d20c6d4705f4db58ba2cbeb004daa8372e0fd2
YoannDaSilvaCastro <yoann.dsc@gmail.com>
2024-07-17 10:26:53 +0200

    fix: :construction: New version of PersonalData
    Correct API link (still unauthorized, Access denied bc token)  + scss OK

--- src/components/Pages/PersonalData/PersonalData.tsx
+++ src/components/Pages/PersonalData/PersonalData.tsx
@@ -1,49 +1,39 @@
 import './PersonalData.scss';
 import { useState, useEffect } from 'react';
 
-interface UserData {
-  name: string;
-  email: string;
-}
-
-function PersonalData() {
-  const [userData, setUserData] = useState<UserData>({
-    name: '',
-    email: '',
-  });
+const PersonalData = () => {
+  const [userData, setUserData] = useState({ name: '', email: '' });
 
   useEffect(() => {
-    async () => {
+    const fetchUserData = async () => {
       try {
         const response = await fetch('https://cado.zapto.org/me');
-        if (response.ok) {
-          const userData = await response.json();
-          setUserData(userData);
-        } else {
-          console.error('Impossilbe de récupérer les données utilisateur');
-        }
+        const data = await response.json();
+        setUserData({ name: data.name, email: data.email });
       } catch (error) {
         console.error(
-          'Erreur à la récupération des données utilisateur',
+          'Erreur lors de la récupération des données utilisateur:',
           error
         );
       }
     };
-  });
+
+    fetchUserData();
+  }, []);
 
   return (
     <div className="WebsiteName">
-      <header className="Website__Title">
+      <header className="PersonalData__Title">
         <h1>Données personnelles</h1>
       </header>
       <div className="PersonalData">
         <div>
-          <p>
+          <h2>
             <strong>Nom:</strong> {userData.name}
-          </p>
-          <p>
+          </h2>
+          <h2>
             <strong>Email:</strong> {userData.email}
-          </p>
+          </h2>
         </div>
         {/* <button onClick={handleSave}>Enregistrer</button> 
         Ce bouton sera utilisé si on met en place les modifications
@@ -51,6 +41,6 @@ function PersonalData() {
       </div>
     </div>
   );
-}
+};
 
 export default PersonalData;
