0db7073df8129207b7e8a19c0786f6e6065e1aa8
YoannDaSilvaCastro <yoann.dsc@gmail.com>
2024-07-17 10:08:08 +0200

    feat: :construction: PersonalData V2 - Fetch API

--- src/components/Pages/PersonalData/PersonalData.tsx
+++ src/components/Pages/PersonalData/PersonalData.tsx
@@ -1,51 +1,50 @@
 import './PersonalData.scss';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 interface UserData {
-  nom: string;
-  prénom: string;
+  name: string;
   email: string;
-  password: string;
 }
 
 function PersonalData() {
   const [userData, setUserData] = useState<UserData>({
-    nom: '',
-    prénom: '',
+    name: '',
     email: '',
-    password: '',
   });
 
   useEffect(() => {
-    const authData = JSON.parse(localStorage.getItem('authData') || '{}');
-    setUserData({
-      nom: authData.nom || '',
-      prénom: authData.prénom || '',
-      email: authData.email || '',
-      password: authData.password || '',
-    });
-  }, []);
+    async () => {
+      try {
+        const response = await fetch('https://cado.zapto.org/me');
+        if (response.ok) {
+          const userData = await response.json();
+          setUserData(userData);
+        } else {
+          console.error('Impossilbe de récupérer les données utilisateur');
+        }
+      } catch (error) {
+        console.error(
+          'Erreur à la récupération des données utilisateur',
+          error
+        );
+      }
+    };
+  });
 
   return (
-    <div className="PersonalData">
-      <header className="PersonalData__Title">
-        <h1 className="PersonalData__h1">Données personnelles</h1>
+    <div className="WebsiteName">
+      <header className="Website__Title">
+        <h1>Données personnelles</h1>
       </header>
-      <div className="PersonalData__details">
-        <p className="PersonalData__item">
-          <strong>Nom:</strong> {userData.nom}
-        </p>
-        <p className="PersonalData__item">
-          <strong>Prénom:</strong> {userData.prénom}
-        </p>
-        <p>
-          <strong>Email:</strong> {userData.email}
-        </p>
-        <p className="PersonalData__item">
-          <strong>Mot de passe:</strong> {userData.password}
-          {/* Afficher le mot de passe en texte brut n'est généralement pas une bonne pratique pour des raisons de sécurité. Même si c'est sur le côté client, cela peut toujours poser des problèmes de sécurité, surtout si l'écran est visible par d'autres personnes. Vous devriez envisager de ne pas afficher le mot de passe ou de le masquer d'une manière ou d'une autre. */}
-        </p>
-
+      <div className="PersonalData">
+        <div>
+          <p>
+            <strong>Nom:</strong> {userData.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {userData.email}
+          </p>
+        </div>
         {/* <button onClick={handleSave}>Enregistrer</button> 
         Ce bouton sera utilisé si on met en place les modifications
         de données personnelles */}
