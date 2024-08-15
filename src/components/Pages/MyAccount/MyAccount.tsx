860383223118da4fd04f5c1454635d4b7361bf92
yanis-lannabi <yasi_l78@hotmail.com>
2024-08-15 14:59:37 +0200

    add loading state

--- src/components/Pages/MyAccount/MyAccount.tsx
+++ src/components/Pages/MyAccount/MyAccount.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 const MyAccount = () => {
   const [user, setUser] = useState('');
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
     const fetchUserData = async () => {
@@ -16,51 +17,60 @@ const MyAccount = () => {
         const data = await response.json();
         console.log('data', data);
         if (response.ok) {
-          setUser(data); // Stockez les informations utilisateur
+          setUser(data);
+          // Stockez les informations utilisateur
         } else {
           setError(data.message);
         }
       } catch (err) {
         setError('An error occurred. Please try again.');
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchUserData();
   }, []);
 
   return (
-    <div className="MyAccount">
-      <header className="MyAccount-title">
-        <h1 className="MyAccount__h1">Mon compte</h1>
-      </header>
-      <p className="MyAccount-WelcomeMessage">
-        Bienvenue {''}
-        {user?.name?.charAt(0).toUpperCase() +
-          user?.name?.slice(1).toLowerCase()}
-        {''} !
-      </p>
-      <div className="MyAccount__Buttons">
-        <button
-          className="MyAccount__personalData"
-          onClick={() => navigate('/mes-donnees-personnelles')}
-        >
-          Données personnelles
-        </button>
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="MyAccount">
+          <header className="MyAccount-title">
+            <h1 className="MyAccount__h1">Mon compte</h1>
+          </header>
+          <p className="MyAccount-WelcomeMessage">
+            Bienvenue {''}
+            {user.name.charAt(0).toUpperCase() +
+              user.name.slice(1).toLowerCase()}
+            {''} !
+          </p>
+          <div className="MyAccount__Buttons">
+            <button
+              className="MyAccount__personalData"
+              onClick={() => navigate('/mes-donnees-personnelles')}
+            >
+              Données personnelles
+            </button>
 
-        <button
-          className="MyAccount__MyEvents"
-          onClick={() => navigate('/mes-evenements')}
-        >
-          Mes évènements
-        </button>
+            <button
+              className="MyAccount__MyEvents"
+              onClick={() => navigate('/mes-evenements')}
+            >
+              Mes évènements
+            </button>
 
-        <button
-          className="MyAccount__CreateEvent"
-          onClick={() => navigate('/creer-un-evenement')}
-        >
-          Créer un évènement
-        </button>
-      </div>
-    </div>
+            <button
+              className="MyAccount__CreateEvent"
+              onClick={() => navigate('/creer-un-evenement')}
+            >
+              Créer un évènement
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default MyAccount;
